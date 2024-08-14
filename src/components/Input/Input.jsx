65ac23c8c603d6364b3193aa042d61892c1f336c
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2024-08-14 16:50:52 +0530

    improving contact page

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,8 +4,8 @@ const Input = ({value, className, ...otherProps}) => {
         <input
             {...otherProps}
             value={value}
-            className={`min-w-[320px] sm:min-w-[350px] w-[90%] border-2 border-[#2a2a2a] p-4 pt-3 pb-3  placeholder:text-[#616161]
-                outline-none ${value ? 'bg-green-900' : 'bg-[#451c32]'} text-white text-xl font-Poppins rounded-sm
+            className={`sm:max-w-[400px] w-[300px] sm:min-w-[350px] sm:w-[90%] border-2 border-[#2a2a2a] p-2 sm:p-4 sm:pt-3  sm:pb-3  placeholder:text-[#616161]
+                outline-none ${value ? 'bg-green-900' : 'bg-[#451c32]'} text-white text-lg sm:text-xl font-Poppins rounded-sm
             focus:ring-[1px] focus:ring-black focus:bg-[#642d19] shadow-[7px_7px_0px_1px_rgb(0,0,0)] ${className}`}
         />
     );
